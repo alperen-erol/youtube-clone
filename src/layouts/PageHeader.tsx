f57050cf0315163b6a1cbe027947ee60a1b7789c
Alperen Erol <alpereneroltr@gmail.com>
2024-09-01 20:22:57 +0300

    header fix

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -12,23 +12,22 @@ export function PageHeader() {
           <img src={logo} className="h-6" />
         </a>
       </div>
-      <div>
-        <form className="flex gap-4 flex-grow justify-center">
-          <div className="flex flex-grow max-w-[600px] ">
-            <input
-              type="search"
-              placeholder="Search"
-              className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"
-            />
-            <Button className="py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0">
-              <Search />
-            </Button>
-          </div>
-          <Button type="button" size="icon" className="flex-shrink-0">
-            <Mic />
+
+      <form className="flex gap-4 flex-grow justify-center">
+        <div className="flex flex-grow max-w-[600px]">
+          <input
+            type="search"
+            placeholder="Search"
+            className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"
+          />
+          <Button className="py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0">
+            <Search />
           </Button>
-        </form>
-      </div>
+        </div>
+        <Button type="button" size="icon" className="flex-shrink-0">
+          <Mic />
+        </Button>
+      </form>
       <div className="flex flex-shrink-0 md:gap-2">
         <Button size="icon" variant="ghost">
           <Upload />
